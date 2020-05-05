6d43fb329fcdbd55ed51d94fb6f7eec9a156d9b3
Darren Meehan <hi@drn.ie>
2020-05-05 19:28:13 +0100

    Add some initial logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate log;
+extern crate simple_logger;
 use lambda::lambda;
 use serde_json::Value;
 
@@ -6,5 +9,10 @@ type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 #[lambda]
 #[tokio::main]
 async fn main(event: Value) -> Result<Value, Error> {
+    simple_logger::init().unwrap();
+    debug!("This is an example debug message.");
+    info!("This is an example info message.");
+    warn!("This is an example warn message.");
+    error!("This is an example error message.");
     Ok(event)
 }
