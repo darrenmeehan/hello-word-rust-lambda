55addd58326ca186daea4a791b6bc2c19e332933
Darren Meehan <hi@drn.ie>
2020-05-05 08:54:36 +0100

    Add a simple script for deployment

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cargo check
+
+cargo test
+
+cargo clippy
+
+cargo build --release --target x86_64-unknown-linux-musl
+
+cp ./target/x86_64-unknown-linux-musl/release/register-on-upload ./bootstrap && zip lambda.zip bootstrap && rm bootstrap
+
+aws lambda update-function-code --function-name registerOnUpload \
+--zip-file fileb://./lambda.zip
+
+aws lambda invoke --function-name registerOnUpload \
+  --cli-binary-format raw-in-base64-out \
+  --payload '{"firstName": "world"}' \
+  output.jsons
+
+cat output.json
